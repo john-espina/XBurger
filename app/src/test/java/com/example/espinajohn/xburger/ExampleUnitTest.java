ce05c70a769f1489aa4516659d390bb6cde188ca
mstlg <mstlg@users.noreply.github.com>
2017-11-07 20:09:28 +1300

    2 unit tests
    Failed

--- app/src/test/java/com/example/espinajohn/xburger/ExampleUnitTest.java
+++ app/src/test/java/com/example/espinajohn/xburger/ExampleUnitTest.java
@@ -1,17 +1,26 @@
-//package com.example.espinajohn.xburger;
-//
-//import org.junit.Test;
-//
-//import static org.junit.Assert.*;
-//
-///**
-// * Example local unit test, which will execute on the development machine (host).
-// *
-// * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
-// */
-//public class ExampleUnitTest {
+package com.example.espinajohn.xburger;
+
+import org.junit.Test;
+
+import api_communicators.Customer;
+import api_communicators.CustomerDetailsController;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
 //    @Test
-//    public void addition_isCorrect() throws Exception {
-//        assertEquals(4, 2 + 2);
+//    public void testCreateCustomerNull() throws Exception {
+//        assertEquals(null, CustomerControls.createCustomer ("Dion"));
 //    }
-//}
+
+//    @Test
+//    public void testCreateCustomerTrue() throws Exception {
+//        String maddie = Customer.getCustomerDetailsAPI ("maddie", "username").getUsername ();
+//        assertEquals (maddie, CustomerControls.createCustomer ("maddie").getUsername ());
+//    }
+}

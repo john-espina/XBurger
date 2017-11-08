233d7650ecb3e1688ea250e6b31ac48d44325f8e
john espina <espinajohn@myvuw.ac.nz>
2017-11-08 15:36:37 +1300

    Updated Cheese java class with methods to determine which ingredients are not available.

--- app/src/main/java/fragments_ingredient_page/CheeseFragment.java
+++ app/src/main/java/fragments_ingredient_page/CheeseFragment.java
@@ -12,6 +12,13 @@ import android.widget.RadioGroup;
 
 import com.example.espinajohn.xburger.R;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
+
+import api_communicators.Stock;
+import api_communicators.StockDetailsController;
+import helpers.StockControls;
 
 
 public class CheeseFragment extends Fragment {
@@ -22,6 +29,9 @@ public class CheeseFragment extends Fragment {
     RadioButton edam;
     RadioButton smoked;
     RadioButton brie;
+    ArrayList<RadioButton> radioButtons;
+    HashMap<String, ArrayList> stocks;
+    ArrayList<Stock> cheeses;
 
     public CheeseFragment() {
         // Required empty public constructor
@@ -40,6 +50,21 @@ public class CheeseFragment extends Fragment {
        edam = (RadioButton) rootView.findViewById(R.id.cheese_edam);
        smoked = (RadioButton) rootView.findViewById(R.id.cheese_smoke);
        brie = (RadioButton) rootView.findViewById(R.id.cheese_brie);
+
+       radioButtons = StockControls.createRadioButtonList(rg);
+
+       try{
+           //if statement here if previosly clicked so wont have to query databse again
+           stocks = new StockDetailsController().execute().get();
+           cheeses = stocks.get("cheeseCategory");
+
+       }catch (InterruptedException e){
+           e.printStackTrace();
+       } catch (ExecutionException e){
+           e.printStackTrace();
+       }
+
+       StockControls.updateStockView(cheeses,radioButtons);
 
        return rootView;
     }
